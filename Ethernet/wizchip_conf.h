c2356c8777b86bb5eb00a5d6e941ed8f0958c4b5
yaozhiman <yzman@163.com>
2018-12-01 20:10:05 +0800

    1.For W5100S,The array size is one byte smaller 2.W5100S also support KA

--- Ethernet/wizchip_conf.h
+++ Ethernet/wizchip_conf.h
@@ -232,7 +232,7 @@ extern "C" {
 typedef struct __WIZCHIP
 {
    uint16_t  if_mode;               ///< host interface mode
-   uint8_t   id[6];                 ///< @b WIZCHIP ID such as @b 5100, @b 5200, @b 5500, and so on.
+   uint8_t   id[7];                 ///< @b WIZCHIP ID such as @b 5100, @b 5200, @b 5500, and so on.
    /**
     * The set of critical section callback func.
     */
