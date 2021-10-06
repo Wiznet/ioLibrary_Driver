f23aeea75db854e5fe92628b95d3e615a64babb4
GiungKim <forever4191@naver.com>
2021-10-06 14:20:33 +0900

    Fixed httpUtil.c error

--- Internet/httpServer/httpUtil.c
+++ Internet/httpServer/httpUtil.c
@@ -63,3 +63,13 @@ uint8_t http_post_cgi_handler(uint8_t * uri_name, st_http_request * p_http_reque
 	if(ret)	*file_len = len;
 	return ret;
 }
+
+uint8_t predefined_get_cgi_processor(uint8_t * uri_name, uint8_t * buf, uint16_t * len)
+{
+	;
+}
+
+uint8_t predefined_set_cgi_processor(uint8_t * uri_name, uint8_t * uri, uint8_t * buf, uint16_t * en)
+{
+	;
+}
