b3e089eaf4e8dfc63c2986dd07424edcb1a32756
seok930927 <tjr0927@gmail.com>
2025-04-22 15:56:49 +0900

    debug recv()

--- Ethernet/socket.c
+++ Ethernet/socket.c
@@ -670,8 +670,13 @@ int32_t recv(uint8_t sn, uint8_t * buf, uint16_t len)//lihan
             return SOCKERR_SOCKSTATUS;
          }
       }
+#ifdef IPV6_AVAILABLE
+      if(recvsize != 0) break;
+      if(sock_io_mode & (1<<sn)) return SOCK_BUSY;
+#else
       if(sock_io_mode & (1<<sn)) return SOCK_BUSY;
       if(recvsize != 0) break;
+#endif 
    };
 #if _WIZCHIP_ == 5300
    }
